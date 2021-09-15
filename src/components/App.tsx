106f16140f0d904e6327f1afa08927bf83d62c58
Astratyan Dmitry <astratyandmitry@gmail.com>
2021-09-15 22:24:22 +0600

    Init

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App () {
+  return (
+    <div className="absolute inset-0 w-full h-full bg-black text-white flex items-center justify-center">
+      <div className="text-5xl font-thin">React.App</div>
+    </div>
+  )
+}
+
+export default App
